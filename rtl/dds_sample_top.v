15d63ca81a17fc7b409a1169c1d55b069bd631f6
姚琳 <yaolin0616@163.com>
2025-05-30 15:44:46 +0800

    发送数据增加crc8校验

--- rtl/dds_sample_top.v
+++ rtl/dds_sample_top.v
@@ -279,7 +279,7 @@ always @(*) begin
         3'd1: uart_tx_data = rev_data1;
         3'd2: uart_tx_data = rev_data2;
         3'd3: uart_tx_data = rev_data3;
-        3'd4: uart_tx_data = 8'h11;
+        3'd4: uart_tx_data = uart_tx_crc8;
         3'd5: uart_tx_data = 8'h55;
         default: uart_tx_data = 8'h00;
     endcase
